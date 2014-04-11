5a75acb0ca9a14cd3f9f59bc1e8999ddb4c2a68e
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-04-10 21:42:54 -0700

    added ember dependency and customer seeds to seeds.rb

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ end
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-
+gem 'ember-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Customer.create(name: 'Newguy McNorthEast', email: 'newguy@gmail.com',
+                telephone: '232-221-2133', cust_notes: 'This guy is pretty bad at boxing')
+Customer.create(name: 'Newlady McPearlDistrict', email: 'newlady@gmail.com',
+                telephone: '232-554-2133', cust_notes: 'Knocked newguy\'s head off at practice')
