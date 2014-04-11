f8344aefc4be86d197268b865ac18c8cd78801ba
twurz <travis.wurzer@yahoo.com>
2014-04-10 22:07:09 -0700

    added routes and customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ActiveRecord::Base
+  has_one :name
+  has_one :email
+  has_one :telephone
+  has_one :cust_notes
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe Customer do
-  it "has a valid factory"
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:customer4564564)).to be_valid
+  end
   it "is invalid without a name"
+
   it "is invalid without an email"
   it "is invalid without a phone number"
-end
-
 end
