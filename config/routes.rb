f8fc9b10a6cd34571dbfc4bf3b040fa2b210d091
twurz <travis.wurzer@yahoo.com>
2014-04-10 23:10:58 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Delucia::Application.routes.draw do
-  resources :customers
-  root to: "home#index"
 
+  resources :trainer do
+    resources :customers, only: [:show, :edit, :update, :destroy]
+  root to: "application#index"
 end
--- spec/models/trainer.rb
+++ spec/models/trainer.rb
@@ -1,2 +1,5 @@
-class Customer < ActiveRecord::Base
+class Trainer < ActiveRecord::Base
+  has_secure_password
+  has_many :customers
+  validates :email, presence: true
 end
